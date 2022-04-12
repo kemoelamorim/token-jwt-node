d79af4d33ca6bcd6861df66432e1f388ee9ba420
Kemoel Amorim Miranda <kemoel.amorim@gmail.com>
2022-04-11 23:10:19 -0300

    JWT com node

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const { estategiasAutenticacao } = require('./src/usuarios')
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+module.exports = app;
